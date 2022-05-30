0073bad117104e60b1c9a0246ec45c3e3e56e7be
unknown <victorinojohnmark@gmail.com>
2022-05-30 16:52:44 +0800

    Dashboard menu updated

--- config/adminlte.php
+++ config/adminlte.php
@@ -227,96 +227,36 @@ return [
     'menu' => [
         // Navbar items:
         [
-            'type'         => 'navbar-search',
-            'text'         => 'search',
-            'topnav_right' => true,
-        ],
-        [
             'type'         => 'fullscreen-widget',
             'topnav_right' => true,
         ],
 
         // Sidebar items:
         [
-            'type' => 'sidebar-menu-search',
-            'text' => 'search',
-        ],
-        [
-            'text' => 'blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
-        ],
-        [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
-        ],
-        ['header' => 'account_settings'],
-        [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
-        ],
-        [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
-        ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
+            'text' => 'Master Records',
+            'icon' => 'fas fa-fw fa-clipboard-list',
             'submenu' => [
                 [
-                    'text' => 'level_one',
+                    'text' => 'Companies',
                     'url'  => '#',
+                    'icon' => 'fas fa-fw fa-building',
                 ],
                 [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
+                    'text' => 'Suppliers',
+                    'url'  => '#',
+                    'icon' => 'fas fa-fw fa-check-circle',
                 ],
                 [
-                    'text' => 'level_one',
+                    'text' => 'Locations',
                     'url'  => '#',
+                    'icon' => 'fas fa-fw fa-map-marked-alt',
                 ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
+                [
+                    'text' => 'Items',
+                    'url'  => '#',
+                    'icon' => 'fas fa-fw fa-boxes',
+                ]
+            ]
         ],
     ],
 
